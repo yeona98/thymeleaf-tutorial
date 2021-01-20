8bfc85f2fa148c0d46a1fc006c8daab3a30d098c
jane <sy4454@naver.com>
2021-01-20 15:59:07 +0900

    thymeleaf basic

---
+++ src/main/java/com/example/thymeleaftutorial/controller/SampleController.java
@@ -0,0 +1,61 @@
+package com.example.thymeleaftutorial.controller;
+
+import com.example.thymeleaftutorial.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex1() {
+        log.info("show /ex1");
+    }
+
+    @GetMapping({"/ex2", "exLink"})
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream()
+                .mapToObj(i -> {
+                    return SampleDTO.builder()
+                            .sno(i)
+                            .first("First"+i)
+                            .last("Last"+i)
+                            .regTime(LocalDateTime.now())
+                            .build();
+                }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping({"/exInline"})
+    public String exInline(RedirectAttributes redirectAttributes) {
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First")
+                .last("Last")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("show /ex3");
+    }
+}
---
+++ src/main/java/com/example/thymeleaftutorial/dto/SampleDTO.java
@@ -0,0 +1,17 @@
+package com.example.thymeleaftutorial.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
+}
