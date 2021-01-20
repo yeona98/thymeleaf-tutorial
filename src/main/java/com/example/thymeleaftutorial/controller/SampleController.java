7ca88db4a00eec88fafe6f80f5a8ebf95965959c
jane <sy4454@naver.com>
2021-01-20 20:59:44 +0900

    thymeleaf layout

--- src/main/java/com/example/thymeleaftutorial/controller/SampleController.java
+++ src/main/java/com/example/thymeleaftutorial/controller/SampleController.java
@@ -58,4 +58,9 @@ public class SampleController {
     public void ex3() {
         log.info("show /ex3");
     }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    public void exLayout1() {
+        log.info("show /exLayout");
+    }
 }
